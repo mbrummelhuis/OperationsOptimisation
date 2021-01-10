a5480ece02c6cc536e655b0ecf844b8febfc69a3
k-vangool <k.van.gool93@gmail.com>
2021-01-10 14:38:56 +0100

    first push

---
+++ Runway_Allocation2.py
@@ -0,0 +1,220 @@
+"""
+Gurobi algorithm to solve the MILP Runway Allocation problem for the AE4441-16 Operation Optimisation assignment.
+
+It takes takes the generated landing requests as input for the solver
+"""
+
+import gurobipy as gp
+import pandas as pd
+from gurobipy import GRB, LinExpr, Model
+import numpy as np
+import numpy.random as rnd
+import time
+import os
+
+# Get path to current folder and select the correct file
+cwd = os.getcwd()
+filename = 'landing_requests.csv'
+
+startTime = time.time()
+
+# Load the flight data
+flights = pd.read_csv(os.path.join(cwd, filename), delimiter=',', skiprows=2, header=0)
+
+model = gp.Model('runway_allocation')
+
+# Initiate multidicts for storage
+runways, runway_headings, runway_pop = gp.multidict({
+    'R18R': [180, 14500],
+    'R06E': [60, 700],
+    'R24W': [240, 13600]
+})
+
+headwind_runways = [h + 180 if h + 180 <= 360 else h - 180 for h in runway_headings.values()]
+
+IAFs, deg_range_IAFs, time_to_r, time_to_taxi = gp.multidict({
+
+    'ARTIP': [[0, 120], [1177, 503, 766], [60, 39, 154]],
+    'RIVER': [[120, 240], [636, 1177, 1261], [60, 39, 154]],
+    'SUGOL': [[240, 360], [709, 1040, 777], [60, 39, 154]]
+
+})
+
+AC_type, AC_fuel, AC_db = gp.multidict({
+    'H': [1.029, 17],
+    'M': [0.242, 7]
+})
+
+'''
+Creating a dataframe which contains:
+    - IAF
+    - Runway
+    - Cost for that arc for that AC
+    - AC
+'''
+cpa = []
+for AC in AC_type:
+    for n in range(len(IAFs)):
+        for r in range(len(runways)):
+            cpa.append([IAFs[n], runways[r], time_to_r[IAFs[n]][r] * AC_fuel[AC], AC])
+
+# Dataframe with the cost per arc
+df_cpa = pd.DataFrame(cpa)
+df_cpa.columns = ['IAF', 'Runway', 'CPA/AC', 'AC']
+
+'''
+Filtering the generated landing request to their respective IAF
+'''
+flights_filtered = flights.copy()
+
+for i in range(len(flights)):
+    for IAF in IAFs:
+        lb = deg_range_IAFs[IAF][0]
+        ub = deg_range_IAFs[IAF][1]
+        if lb <= flights['approach direction'][i] < ub:
+            flights_filtered.loc[i, 'approach direction'] = IAF
+
+'''
+Creating runway compatibility based on wind direction and runway heading
+'''
+runway_comp = np.zeros((len(flights['wind direction']), len(runways)))
+
+for w in range(len(flights['wind direction'])):
+    for h in range(len(headwind_runways)):
+        ub = headwind_runways[h] + 90
+        lb = headwind_runways[h] - 90
+        if lb <= flights['wind direction'][w] < ub:
+            runway_comp[w, h] = 1
+
+'''
+Setting up the decision variables:
+    > flight_{f, r, d}: arriving flight from IAF f to runway r, with delay d
+    > G_{n}: noise penalty
+'''
+
+x = {}
+G = {}
+
+delay = 0
+
+for f in range(len(flights['approach direction'])):
+    for r in range(len(runways)):
+        x[f, r] = model.addVar(lb=0, ub=0, vtype=GRB.BINARY, name='x%s%s' % (f, r))
+
+for r in range(len(runways)):
+    G[r] = model.addVar(lb=0, ub=1, vtype=GRB.BINARY, name='G%s' % (r))
+
+model.update()
+
+'''
+Adding the constraints:
+    > Always Assign Flight: make sure that every AC gets assigned
+    > Wind Restriction: AC should choose to land at runway where the wind is opposite of the heading
+    > AC Landing sep: make sure that there is enough time between consecutive arrivals
+    > Noise limit switching: when a certain noise limit is surpassed, this constraint files an extra penalty
+'''
+
+# Always Assign Flight Constraint
+
+for f in range(len(flights['approach direction'])):
+    AAS_LHS = LinExpr()
+    for r in range(len(runways)):
+        AAS_LHS += x[f, r]
+
+    model.addConstr(lhs=AAS_LHS, sense=GRB.EQUAL, rhs=1, name=f'Flight_f{f}')
+
+# Wind Restriction Constraint
+for f in range(len(flights)):
+    for r in range(len(runways)):
+        WR_LHS = LinExpr()
+
+        if runway_comp[f, r]:
+            WR_LHS += runway_comp[f, r] * x[f, r]
+
+            model.addConstr(lhs=WR_LHS, sense=GRB.EQUAL, rhs=1, name=f'WR_f{f}_r{r}')
+
+# AC Time Separation Constraint
+T_sep = 120  # Time separation in seconds
+
+for f1 in range(len(flights['time in seconds'])):
+    for f2 in range(len(flights['time in seconds'])):
+
+        # TODO check if the time of arrival for both flights is close to eachother.
+
+        if f1 != f2:  # Only calculate when it concerns two different flights
+
+            for r1 in range(len(runways)):
+                for r2 in range(len(runways)):
+
+                    if r1 == r2:  # Only calculate if they might land on the same runway
+                        AC_sep_LHS = LinExpr()
+
+                        # Time separation is arrival time of f2 - arrival time of f1 + taxi time f1
+                        AC_sep_LHS += (flights['time in seconds'][f2] - flights['time in seconds'][f1] +
+                                       time_to_taxi[flights_filtered['approach direction'][f1]][r1]) * x[f, r1]
+
+                        model.addConstr(lhs=AC_sep_LHS, sense=GRB.GREATER_EQUAL, rhs=T_sep, name=f'AC_sep_f{f1}_f{f2}_r{r1}')
+
+# Noise limit switching constraint
+# TODO: right now this does nothing with the noise levels generated by the AC type > only in the cost func
+N_limit = 4000
+
+for f in range(len(flights['approach direction'])):
+    for r in range(len(runways)):
+        NLSC_LHS = LinExpr()
+
+        big_M = 100000
+
+        NLSC_LHS += runway_pop[runways[r]] * x[f, r] - big_M * G[r]
+
+        model.addConstr(lhs=NLSC_LHS, sense=GRB.LESS_EQUAL, rhs=N_limit, name=f'NLSC_f{f}_r{r}_G{r}')
+
+model.update()
+
+'''
+Generating the cost function
+ > Minimize the cost
+'''
+
+alpha = 0.3
+beta = 1 - alpha
+
+n_f = 1
+n_n = 1
+
+obj_func = LinExpr()
+
+for t in AC_type:
+    for f in range(len(flights_filtered['approach direction'])):
+        for r in range(len(runways)):
+            C_F = df_cpa.loc[(df_cpa['IAF'] == flights_filtered['approach direction'][f]) & (df_cpa['AC'] == t)
+                             & (df_cpa['Runway'] == runways[r]), 'CPA/AC'].values[0]
+
+            obj_func += C_F * x[f, r]
+
+for t in AC_type:
+    for r in range(len(runways)):
+        C_N = AC_db[t]
+
+        obj_func += C_N * G[r]
+
+model.update()
+model.setObjective(obj_func, GRB.MINIMIZE)
+
+model.update()
+model.write('model_formulation.lp')
+
+model.optimize()
+model.computeIIS()
+
+# model.remove(gp.Constr())
+
+# model.remove(gp.Constr(AAS_LHS[0]))
+model.update()
+model.write('model_delete_var.lp')
+model.optimize()
+model.computeIIS()
+
+endTime = time.time()
+
+model.write('model.ilp')
---
+++ datageneration_new.py
@@ -0,0 +1,78 @@
+5"""
+Data generation algorithm for AE4441-16 Operations Optimisation assignment
+Generating random input data for the optimisation algorithm
+
+Creates a .csv file in the file directory with the specified number of data points
+The user inputs to this are:
+    Amount of data points
+    The flight frequency in average flights per hour
+
+Each data point represents an aircraft requesting landing at an arbitrary airport.
+For each aircraft, the following data is available:
+    Time of coming in (requesting the landing at tower)
+    Aircraft type (class based on weight)
+    Include direction of approach
+    
+    - Calculate time to sensible format
+    - for some reason it skips each other row, fix this
+"""
+import csv
+import random
+import numpy as np
+
+def convertTime(seconds): 
+    day = seconds // (24 * 3600)
+    seconds %= (24 * 3600) 
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    
+    time = str(day) + ":" + format(hour, '02d') + ":" + format(minutes, '02d') + ":" + format(seconds, '02d')
+      
+    return time
+
+time_seconds = 0
+headings = np.arange(0,360,1)
+
+
+#Division between Medium and Heavy aircraft (sum to 1)
+medium = 0.7
+heavy = 0.3
+
+print("Data generation for AE4441-16 Operations optimisation.")
+flights = int(input("Please enter the amount of data points (flights): "))
+flight_frequency = input("Please enter the average flight frequency (flights/hr): ")
+
+mean_time_between_flights = float(3600)/float(flight_frequency)
+stdev_time_between_flights = float(mean_time_between_flights)/float(4)
+
+#Initializing csv file
+csv_file = open('landing_requests.csv', 'w')
+
+with open('landing_requests.csv','w') as data_file:
+    writer = csv.writer(data_file,lineterminator = '\n')
+    
+    writer.writerow(['amount of flights',flights])
+    writer.writerow(['average flights per hour', flight_frequency])
+
+    writer.writerow(['time in seconds', 'time in d:hh:mm:ss', 'category', 'approach direction', 'wind direction'])
+    for flight in range(flights):
+        time_since_last_flight = np.random.normal(mean_time_between_flights, stdev_time_between_flights)
+        time_seconds = int(time_seconds + time_since_last_flight)
+        
+        category_probability = random.random()
+        if category_probability < medium:
+            category = 'Medium'
+        else:
+            category = 'Heavy'
+
+        approach_direction = random.choice(headings)
+        
+        wind_direction = random.choice(headings)
+        
+        time_hhmmss = convertTime(time_seconds)
+        
+        writer.writerow([time_seconds, time_hhmmss, category, approach_direction, wind_direction])
+
+print('Done')
